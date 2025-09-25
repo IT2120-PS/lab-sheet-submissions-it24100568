22165c8913cd9ccc25e1927a55458e8b18f7ec5d
it24100568 <it24100568@my.sliit.lk>
2025-09-25 11:03:36 +0530

    Add files via upload

---
+++ Lab Sheet 06/IT24100568_LAB06.R
@@ -0,0 +1,22 @@
+setwd("C:\\Users\\levin\\Documents\\IT24100568")
+
+#EXERSICE 1 
+# P = 0.85 n=50 
+#(i)
+#BINOMIAL distribution 
+
+#(ii)
+1- pbinom(46,50,0.85,lower.tail = TRUE)
+
+
+#(2)
+#(i)
+#number of Customer calls per hour.
+
+#(ii)
+#Poisson distribution
+
+#(iii)
+dpois(15,12)
+
+
