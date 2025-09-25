38fa3731a2863e381a1e4a57463ae3508010dc31
it24100568 <it24100568@my.sliit.lk>
2025-09-25 12:22:20 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24100568_LAB07.R
@@ -0,0 +1,11 @@
+setwd("C:\\Users\\levin\\Documents\\IT24100568")
+
+#Question 1
+1-punif(10,min=0,max=40,lower.tail=TRUE)-punif(25,min=0,max=40,lower.tail=FALSE)
+
+#Question 2
+pexp(2,rate=0.3333333,lower.tail=TRUE)
+
+#Question 3
+pnorm(131,mean=100,sd=15,lower.tail=FALSE)
+qnorm(0.95,mean=100,sd=15,lower.tail=TRUE)
